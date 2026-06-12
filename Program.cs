7266be666492f55f50427f0dff6ee55a364418f3
Joelma <joelma.carmo.duarte@gmail.com>
2026-06-12 20:42:55 -0300

    Exercício Teste concluído

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿//Executando
+ExibirOlaMundo();
+ExibirMensagemEmAzul("Olá, meu nome é Zezinho");
+
+
+string nomeDoUsuario = LerNomeDoUsuario();
+
+
+ExibirMensagemEmAzul($"Seu nome é {nomeDoUsuario}");
+
+
+//Declarando
+void ExibirOlaMundo()
+{
+   ExibirMensagemEmAzul("Hello, World!"); 
+}
+
+
+void ExibirMensagemEmAzul(string mensagem)
+{
+    Console.ForegroundColor = ConsoleColor.DarkBlue; 
+    Console.WriteLine(mensagem);
+    Console.ResetColor();
+}
+
+
+string LerNomeDoUsuario()
+{
+    Console.Write("Informe seu nome: ");
+    string nome = Console.ReadLine()!;
+    return nome;
+}
